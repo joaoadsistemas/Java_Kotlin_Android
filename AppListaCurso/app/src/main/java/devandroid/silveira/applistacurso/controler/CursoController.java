19a75d3aa71763922c96f5bef592f69528938622
Joao Henrique <joaoadsistemas@gmail.com>
2023-07-07 12:22:29 -0300

    Arquitetura MVC

---
+++ AppListaCurso/app/src/main/java/devandroid/silveira/applistacurso/controler/CursoController.java
@@ -0,0 +1,4 @@
+package devandroid.silveira.applistacurso.controler;
+
+public class CursoController {
+}
--- AppListaCurso/app/src/main/java/devandroid/silveira/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/silveira/applistacurso/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.silveira.applistacurso;
+package devandroid.silveira.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.silveira.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
