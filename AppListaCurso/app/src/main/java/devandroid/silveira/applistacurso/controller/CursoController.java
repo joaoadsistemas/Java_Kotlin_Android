c9f6ca75b289d9075f015712fed5886f23d96b75
Joao Henrique <joaoadsistemas@gmail.com>
2023-07-14 20:20:25 -0300

    Clonagem e início App Gasolina X Etanol

--- AppListaCurso/app/src/main/java/devandroid/silveira/applistacurso/controller/CursoController.java
+++ AppListaCurso/app/src/main/java/devandroid/silveira/applistacurso/controller/CursoController.java
@@ -10,9 +10,16 @@ public class CursoController {
     public List<Curso> getListaDeCursos() {
         List<Curso> listCursos = new ArrayList<Curso>();
 
-        listCursos.add(new Curso("JAVA"));
+        listCursos.add(new Curso("Java"));
         listCursos.add(new Curso("Dart"));
+        listCursos.add(new Curso("JavaScript"));
         listCursos.add(new Curso("Flutter"));
+        listCursos.add(new Curso("C#"));
+        listCursos.add(new Curso("HTML"));
+        listCursos.add(new Curso("CSS"));
+        listCursos.add(new Curso("SpringBoost"));
+        listCursos.add(new Curso("React"));
+        listCursos.add(new Curso("Angular"));
 
 
         return listCursos;
