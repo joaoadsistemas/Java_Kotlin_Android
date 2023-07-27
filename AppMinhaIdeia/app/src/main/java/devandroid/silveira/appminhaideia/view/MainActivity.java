a08a2677f53b6b49d201602c93ba27b7890ab746
Joao Henrique <joaoadsistemas@gmail.com>
2023-07-26 22:28:12 -0300

    Core AppUtil (Dados e métodos reutilizaveis)

---
+++ AppMinhaIdeia/app/src/main/java/devandroid/silveira/appminhaideia/view/MainActivity.java
@@ -0,0 +1,37 @@
+package devandroid.silveira.appminhaideia.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import devandroid.silveira.appminhaideia.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtNome;
+    TextView txtEmail;
+    TextView txtSexo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        txtNome = findViewById(R.id.txtNome);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSexo = findViewById(R.id.txtSexo);
+
+        Bundle bundle = getIntent().getExtras();
+
+        //a
+
+        txtNome.setText(bundle.getString("nome"));
+        txtEmail.setText(bundle.getString("email"));
+        txtSexo.setText((bundle.getString("sexo")));
+
+
+    }
+}
