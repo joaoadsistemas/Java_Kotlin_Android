95ef28eaa27a93475c4daa12d60dc3112501152a
Joao Henrique <joaoadsistemas@gmail.com>
2023-08-09 17:12:11 -0300

    Método insert no AppDataBase implementado

---
+++ AppBancoDeDadosMeusClientes/app/src/main/java/devandroid/silveira/appbancodedadosmeusclientes/Cliente.java
@@ -0,0 +1,72 @@
+package devandroid.silveira.appbancodedadosmeusclientes;
+
+public class Cliente {
+
+    private String nome;
+    private double salario;
+    private double preco;
+    private int idade;
+    private int ativo;
+    private String dataCadastro;
+    private String horaCadastro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(int ativo) {
+        this.ativo = ativo;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getHoraCadastro() {
+        return horaCadastro;
+    }
+
+    public void setHoraCadastro(String horaCadastro) {
+        this.horaCadastro = horaCadastro;
+    }
+
+
+
+
+}
