b4a4d8c4fcd9cbb82f7918710f3a5017d406c7e3
Joao Henrique <joaoadsistemas@gmail.com>
2023-08-15 23:18:36 -0300

    Clonagem do Drawable para inicio de projeto

---
+++ AppMeusClientes/app/src/main/java/devandroid/silveira/appmeusclientes/controller/ProdutoController.java
@@ -0,0 +1,58 @@
+package devandroid.silveira.appmeusclientes.controller;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import devandroid.silveira.appminhaideia.datasource.AppDataBase;
+import devandroid.silveira.appminhaideia.model.Produto;
+
+public class ProdutoController extends AppDataBase implements ICrud<Produto> {
+
+    ContentValues dadoDoObjeto;
+
+    public ProdutoController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void incluir(Produto obj) {
+
+        dadoDoObjeto = new ContentValues();
+
+        dadoDoObjeto.put("marca", obj.getMarca());
+        dadoDoObjeto.put("produto",obj.getProduto());
+
+        insert("Produto", dadoDoObjeto);
+
+
+    }
+
+    @Override
+    public void deletar(int id) {
+
+        deleteByID("Produto", id);
+    }
+
+    @Override
+    public List<Produto> listar() {
+
+        List<Produto> lista = new ArrayList<>();
+
+        return lista;
+    }
+
+    @Override
+    public void alterar(Produto obj) {
+        dadoDoObjeto = new ContentValues();
+
+        dadoDoObjeto.put("marca", obj.getMarca());
+        dadoDoObjeto.put("produto",obj.getProduto());
+        dadoDoObjeto.put("id", obj.getId());
+
+
+        update("Produto", dadoDoObjeto);
+    }
+}
---
+++ AppMeusClientes/app/src/main/java/devandroid/silveira/appmeusclientes/core/AppUtil.java
@@ -0,0 +1,13 @@
+package devandroid.silveira.appmeusclientes.core;
+
+public class AppUtil {
+
+public static String versaoDoAplicativo(){
+    return "Versão 1.1";
+};
+public static final String TAG = "DB_Crud";
+
+}
+
+
+
